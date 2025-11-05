0d3f194c16a4b08fd464c3bcbc7abb5d2f6d5109
comdobby <tofk1107@gmail.com>
2025-11-05 13:08:17 +0100

    write rep, mirror functions

---
+++ SWTPP/week3/Aufgabe1.hs
@@ -0,0 +1,15 @@
+-- schriebt rep Funktion
+-- z.B.: rep [1,2] 2 -> [1,2,1,2]
+rep :: [a] -> Int -> [a]
+rep _ 0 = []
+rep xs 1 = xs
+rep xs n = xs ++ rep xs (n - 1)
+
+-- schreibt mirror Funktion
+-- z.B.: [1.2] -> [1,2,2,1]
+mirror :: [a] -> [a]
+mirror [] = []
+mirror xs = xs ++ mirror' xs
+    where
+        mirror' [] = []
+        mirror' (x:xs) = mirror' xs ++ [x]

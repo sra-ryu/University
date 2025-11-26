b29ed9fbc3748ec23550418a3b6f1ca9164137a5
comdobby <tofk1107@gmail.com>
2025-11-26 13:08:59 +0100

    add unit test with hspec

--- SWTPP/week5/casino/test/Spec.hs
+++ SWTPP/week5/casino/test/Spec.hs
@@ -1,2 +1,8 @@
+import Test.Hspec
+import Casino(Command(..), toCommand)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "test put chips" $ do
+        it "add two numbers and subtract the fee." $ do
+            Put (Val 10) (Val 5) `shouldBe` Val 14

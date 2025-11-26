a13dbb0044112e826758fbc3ad615e66d762a492
comdobby <tofk1107@gmail.com>
2025-11-26 13:18:34 +0100

    grouping tests by function

--- SWTPP/week5/casino/test/Spec.hs
+++ SWTPP/week5/casino/test/Spec.hs
@@ -5,4 +5,15 @@ main :: IO ()
 main = hspec $ do
     describe "test put chips" $ do
         it "add two numbers and subtract the fee." $ do
-            Put (Val 10) (Val 5) `shouldBe` Val 14
+            Put (Val 10) (Val 5) `shouldBe` 14
+        
+        it "handles zero" $ do
+            Put (Val 0) (Val 5) `shouldBe` 4
+    
+    describe "test take chips" $ do
+        it "substact chips from owned" $ do
+            Take (Val 10) (Val 3) `shouldBe` 7
+        
+        it "returns 0 when taking more than owned" $ do
+            Take (Val 0) (Val 4) `shouldBe` 0
+        

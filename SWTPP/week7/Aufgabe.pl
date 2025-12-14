03baa99b5876aaaa51e7a6d20be4ddaf576c989d
comdobby <tofk1107@gmail.com>
2025-12-14 13:57:38 +0100

    refactor: change Functor name, connect -> edge

--- SWTPP/week7/Aufgabe.pl
+++ SWTPP/week7/Aufgabe.pl
@@ -1,52 +1,52 @@
 /* FAKTEN */
-connect(hamburg, berlin, 300).
-connect(berlin, muenchen, 700).
-connect(hamburg, koeln, 500).
-connect(koeln, frankfurt, 300).
-connect(frankfurt, muenchen, 300).
-connect(frankfurt, stuttgart, 250).
-connect(stuttgart, muenchen, 200).
+edge(hamburg, berlin, 300).
+edge(berlin, muenchen, 700).
+edge(hamburg, koeln, 500).
+edge(koeln, frankfurt, 300).
+edge(frankfurt, muenchen, 300).
+edge(frankfurt, stuttgart, 250).
+edge(stuttgart, muenchen, 200).
 
 /* Regeln */
-directConnection(X,Y,S) :- connect(X,Y,S) ; connect(Y,X,S).
+directedgeion(X,Y,S) :- edge(X,Y,S) ; edge(Y,X,S).
 
-dayTrip(X,Y,Max,S) :- directConnection(X,Y,S), S =< Max.
+dayTrip(X,Y,Max,S) :- directedgeion(X,Y,S), S =< Max.
 
-oneStopConnection(A,B,L) :- directConnection(A,VIA,L1),
-                            directConnection(VIA,B,L2), L is L1+L2.
+oneStopedgeion(A,B,L) :- directedgeion(A,VIA,L1),
+                            directedgeion(VIA,B,L2), L is L1+L2.
 
-shortestOneStop(A,B, VIA,L) :- directConnection(A,VIA,L1),
-                               directConnection(VIA,B,L2), L is L1+L2,
-                               \+(oneStopConnection(A,B,LL), LL < L).
+shortestOneStop(A,B, VIA,L) :- directedgeion(A,VIA,L1),
+                               directedgeion(VIA,B,L2), L is L1+L2,
+                               \+(oneStopedgeion(A,B,LL), LL < L).
 
-pretty(X,Y) :- printConnection(X,Y).
-printConnection(X,Y) :- directConnection(X,Y,S),
+pretty(X,Y) :- printedgeion(X,Y).
+printedgeion(X,Y) :- directedgeion(X,Y,S),
                         write(X), write(" -> "), write(Y), 
                         write(": "), write(S).
 
 dump(X,Y) :- pretty(X,Y), write("\n"), false.
 
-/* A. Predikat connection(A, B) -> prüft, ob zwei Städte über beliebig viele Stopps miteinander verbunden sind. */
-connection(A,B) :- directConnection(A,B,_).
-connection(A,B) :- directConnection(A,C,_), connection(C,B).
+/* A. Predikat edgeion(A, B) -> prüft, ob zwei Städte über beliebig viele Stopps miteinander verbunden sind. */
+edgeion(A,B) :- directedgeion(A,B,_).
+edgeion(A,B) :- directedgeion(A,C,_), edgeion(C,B).
 
 /* B. Print die Anzahl der Städte auf der Route.  */
-countRec(A,B,Count) :- directConnection(A,B,_), C is Count +2,
+countRec(A,B,Count) :- directedgeion(A,B,_), C is Count +2,
                        write("Weg mit "), write(C), write(" Orten gefunden.").
-countRec(A,B,Count) :- directConnection(A,C,_) Count1 is Count + 1,
+countRec(A,B,Count) :- directedgeion(A,C,_) Count1 is Count + 1,
                        countRec(C,B,Count1).
 
 count(A,B) :- countRec(A,B,0).
 
 /* C. Berechnen die Distanz zwischen A und B. */
-distance(A,B,Dist) :- directConnection(A,B,Dist).
-distance(A,B,Dist) :- directConnection(A,C,S), distance(C,B,Dist1),
+distance(A,B,Dist) :- directedgeion(A,B,Dist).
+distance(A,B,Dist) :- directedgeion(A,C,S), distance(C,B,Dist1),
                       Dist is Dist1 + S.
 
 /* D. Print alle Städte zwischen X und Y. */
-route(X,Y) :- directConnection(X,Y,_),
+route(X,Y) :- directedgeion(X,Y,_),
               write(Y), write(" "), write(X).
-route(X,Z) :- directConnection(X,Y,_), route(Y,Z), 
+route(X,Z) :- directedgeion(X,Y,_), route(Y,Z), 
               write(" "), write(X).
 
 /* E. prüfen, ob X eine Primzahl ist. */

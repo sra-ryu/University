aab948f78d3029008f643d0fb13b79cfc1234f93
Saera Ryu <ryu@mac.speedport.ip>
2026-06-20 19:12:14 +0200

    feat: changed API requests to be attempted only 3 times each

--- Data_Science_in_Python_and_R/project/collect.py
+++ Data_Science_in_Python_and_R/project/collect.py
@@ -8,60 +8,39 @@ import time
 
 BASE_URL = "https://v6.bvg.transport.rest"
 
-def get_station_id(search_term):
-    url = f"{BASE_URL}/locations"
-    params = {
-        "query": search_term,
-        "results": 1
-    }
-    
-    response = requests.get(url, params=params)
-    
-    if response.status_code != 200:
-        print(f"API Error: HTTP {response.status_code}")
-        return None
-    
-    data = response.json()
-    if not data:
-        print("No results found.")
-        return None
-
-    station = data[0]
-    print(f"Found: {station['name']} -> ID: {station['id']}")
-    return station['id']
-
 def get_bus_delay(stop_id, line_name, output_csv="bus_delays.csv"):
     url = f"{BASE_URL}/stops/{stop_id}/departures"
     params = {"duration": 10}
 
     departures = []
-    # If an error occurs, it attempts to connect for next 30 seconds.
-    deadline = time.time() + 30
-    attempt = 0
     
-    while time.time() < deadline:
-        attempt += 1
+    # retry 3 times if request fails
+    for attempt in range(3):
         try:
             response = requests.get(url, params=params, timeout=10)
+            response.raise_for_status()
+
             data = response.json()
             departures = data.get("departures", [])
-            if departures:
-                break
-            print(f"Attempt {attempt}: no departures, retrying...")
+            break
+
         except Exception as e:
-            print(f"Attempt {attempt}: {e}, retrying...")
-        
-        wait = min(10, deadline - time.time())
-        if wait > 0:
-            time.sleep(wait)
+            print(f"Attempt {attempt + 1}: {e}")
+
+            if attempt < 2:
+                time.sleep(5)
+
     else:
-        print(f"Skipping stop {stop_id} after 1 minute of retries")
+        print(f"Skipping stop {stop_id}")
         return None
-    
-    filtered = [d for d in departures if d.get("line", {}).get("name") == line_name]
-    
+
+    filtered = [
+        d for d in departures
+        if d.get("line", {}).get("name") == line_name
+    ]
+
     if not filtered:
-        print(f"No depatures found for {line_name}")
+        print(f"No departures found for {line_name} at stop {stop_id}")
         return None
 
     rows = [{
@@ -86,23 +65,44 @@ def get_bus_delay(stop_id, line_name, output_csv="bus_delays.csv"):
     print(df[["line", "direction", "planned_when", "delay_sec"]])
     print(f"Saved to {output_csv}")
 
-# sample
-stops = ["S+U Rathaus Spandau", "U Ruhleben", "S Westend", "U Ernst-Reuter-Platz", "S+U Zoologischer Garten"]
 
-stop_ids = {}
-for stop_name in stops:
-    stop_id = get_station_id(stop_name)
-    if stop_id:
-        stop_ids[stop_name] = stop_id
+""" BUS LINES """
+BUS_LINES = {
+    "140": ['900068202', '900068253', '900068102', '900017101', '900016101', 
+            '900012152', '900013103', '900013151', '900013102', '900014153'],
+    "120": ['900096314', '900096193', '900096101', '900096454', '900085104', 
+            '900085152', '900011151', '900009103', '900009104', '900008107'],
+    "194": ['900075103', '900190506', '900190001', '900120003', '900160001', 
+            '900160534', '900161512', '900181002', '900171002', '900171539'],
+    "M11": ['900066103', '900066101', '900064301', '900064357', '900072101', 
+            '900072103', '900080651', '900082202', '900083181', '900194526'],
+    "M29": ['900048153', '900040101', '900023302', '900023351', '900056101', 
+            '900005105', '900012101', '900012104', '900014101', '900015103'],
+    "M41": ['900077156', '900077155', '900077106', '900076153', '900078105', 
+            '900016252', '900016103', '900012103', '900100020', '900100022'],
+    "M45": ['900027252', '900029302', '900031101', '900025202', '900025252', 
+            '900020234', '900022103', '900022202', '900023101', '900023172'],
+    "M48": ['900049241', '900050251', '900066105', '900062202', '900061101', 
+            '900054105', '900054109', '900056104', '900005205', '900100022'],
+    "X54": ['900130014', '900141002', '900140002', '900140018', '900150022', 
+            '900152001', '900152502', '900170002', '900170001', '900175554'],
+    "X69": ['900170003', '900170510', '900170013', '900171010', '900171006', 
+            '900175008', '900180001', '900180536', '900180020', '900180527']
+}
 
-print(stop_ids)
-
-BASE_DIR = "/Users/ryu/Desktop/study/0_Sose2026/Data Science in Python and R/team/data" # set path of directory
+BASE_DIR = "" # set path of directory
 os.makedirs(BASE_DIR, exist_ok=True)
 
 line_name = "M45"
 output = os.path.join(BASE_DIR, f"bus_delays_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv")
 
-for stop_name, stop_id in stop_ids.items():
-    print(f"\n--- {stop_name} ---")
-    get_bus_delay(stop_id, line_name, output_csv=output)
+# print for test
+for line_name, stop_ids in BUS_LINES.items():
+    print(f"\n=== Line {line_name} ===")
+    for stop_id in stop_ids:
+        print(f"--- {stop_id} ---")
+        get_bus_delay(stop_id, line_name, output_csv=output)
+        
+# for line_name, stop_ids in BUS_LINES.items():
+#     for stop_id in stop_ids:
+#         get_bus_delay(stop_id, line_name, output_csv=output)
